a58520d46139cd17fe8e842e78dd8da0ad7533f4
Vedrana <vedrana_ravlic16@yahoo.com>
2018-10-22 10:51:54 +0200

    Added page reload and getting random win num

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 //UI elements
@@ -15,6 +15,13 @@ const game = document.querySelector("#game"),
 //Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+//Play again event listener
+game.addEventListener("mousedown", function(e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
 
 //Listen for guess
 guessBtn.addEventListener("click", function() {
@@ -65,7 +72,6 @@ function gameOver(won, msg) {
 
   //Disable the input
   guessInput.disabled = true;
-  guessBtn.disabled = true;
 
   //Change the border color
   guessInput.style.borderColor = color;
@@ -73,6 +79,15 @@ function gameOver(won, msg) {
 
   //Set message
   setMessage(msg);
+
+  //Play again?
+  guessBtn.value = "Play Again";
+  guessBtn.className += "play-again";
+}
+
+//Get winning number
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 //Set message
